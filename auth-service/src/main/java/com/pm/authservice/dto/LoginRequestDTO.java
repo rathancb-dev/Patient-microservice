ffa2a5bef8c594ff3aa0306d0e56df2063fe20c1
rathancb-dev <rathancb.dev@gmail.com>
2026-04-05 11:11:05 +0530

    Implemented auth and crated docker-compose file

---
+++ auth-service/src/main/java/com/pm/authservice/dto/LoginRequestDTO.java
@@ -0,0 +1,32 @@
+package com.pm.authservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Email(message = "Password must be at least 8 characters long")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
